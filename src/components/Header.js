47612375616471aeebbcc211484475ab0cb75326
Tobi Weinstock <tv.weinstock@gmail.com>
2016-11-08 18:36:37 +0100

    add header with links

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,26 @@
 import React from 'react';
 
- class Header extends React.Component {
-   constructor() {
-
+class Header extends React.Component {
+   render () {
+     return (
+       <header>
+        <nav>
+          <ul>
+            <NavItem title="about" />
+            <NavItem title="contact" />
+          </ul>
+        </nav>
+       </header>
+     )
    }
- }
+}
+
+const NavItem = (props) => {
+  return (
+    <li>
+      <a href={`/${props.title}`}>{props.title}</a>
+    </li>
+  )
+}
+
+export default Header;
