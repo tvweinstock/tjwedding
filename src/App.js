0492ea6bcb82d6f4213153fde129e3af3dea6d19
Tobi Weinstock <tv.weinstock@gmail.com>
2016-11-08 17:42:35 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import Header from './components/Header';
+// import './App.css';
+import './css/style.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+
+      </div>
+    );
+  }
+}
+
+export default App;
