60bc1eae6d3f70f70c73d8cd1cffb2bcecac6947
tvweinstock <tv.weinstock@gmail.com>
2017-01-11 09:15:28 +0100

    desktop invite

--- src/components/WavePattern.js
+++ src/components/WavePattern.js
@@ -1,5 +1,5 @@
 import React from 'react';
-
+// https://jsfiddle.net/adamculpepper/vWrx3/
 const WavePattern = (props) => {
   return (
     <svg xmlns="http://www.w3.org/2000/svg" version="1.1">
--- src/templates/Main.js
+++ src/templates/Main.js
@@ -6,12 +6,14 @@ class Main extends React.Component {
   render() {
     const textData = this.props.data;
     return (
-      <section id="home" className="invitation">
-        <p>{textData.inviteTitle}</p>
-        <h2>{textData.inviteNames}</h2>
-        <p>{textData.inviteDate}</p>
-        <p>{textData.inviteAddress}</p>
-        <p>{textData.inviteConclusion}</p>
+      <section id="home">
+        <div className="invitation">
+          <p>{textData.inviteTitle}</p>
+          <h2>{textData.inviteNames}</h2>
+          <p>{textData.inviteDate}</p>
+          <p>{textData.inviteAddress}</p>
+          <p>{textData.inviteConclusion}</p>
+        </div>
         <WavePattern />
       </section>
     );
