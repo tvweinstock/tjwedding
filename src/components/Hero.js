6ee84ab9637107a780b74f2c92df1ebfe202bbd4
tvweinstock <tv.weinstock@gmail.com>
2017-02-23 22:54:36 +0100

    hero styles

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -8,9 +8,8 @@ class Hero extends React.Component {
   filterIt() {
     const heroAsset = document.querySelector('.hero__asset');
     const heroAssetHeight = document.querySelector('.hero__asset').clientHeight;
-    const filterAt = (window.scrollY + window.innerHeight) - heroAssetHeight/ 2;
+    const filterAt = (window.scrollY + window.innerHeight) - heroAssetHeight/ 1.5;
     const isHalfScrolled = filterAt > heroAssetHeight / 1.5;
-    console.log(isHalfScrolled);
     if (isHalfScrolled) {
       heroAsset.classList.remove('filtered');
     } else {
@@ -23,8 +22,9 @@ class Hero extends React.Component {
       <div className="hero">
         <div className="hero__asset filtered">
           <div className="hero__title-group">
-            <h1>Tobi + Jeoff <br/>
-            20 / 05 / 2017</h1>
+            <h1 className="hashtag">#jobi2017</h1>
+            <h1>Tobi + Jeoff </h1>
+            <h2>20 / 05 / 2017</h2>
           </div>
         </div>
       </div>
