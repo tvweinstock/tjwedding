9279c0bb7e85873720af62a491c2a4476fa09b06
Tobi Weinstock <tv.weinstock@gmail.com>
2017-03-05 09:05:35 +0100

    changes for gh pages

--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { Miss } from 'react-router';
+import { BrowserRouter } from 'react-router';
 
 import './css/style.css'
 import App from './templates/App';
 import NotFound from './templates/NotFound';
 
+const repo = `/${window.location.pathname.split('/')[1]}`
 const Root = (props) => {
   return (
-    <div>
-      <App />
-      <Miss component={NotFound}/>
-    </div>
+    <BrowserRouter basename={repo}>
+      <div>
+        <App />
+        <Miss component={NotFound}/>
+      </div>
+    </BrowserRouter>
   )
 }
 
