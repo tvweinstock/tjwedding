9517eccf3d7996034ecdebf4efcfb993d0f05b1d
Tobi Weinstock <tv.weinstock@gmail.com>
2017-03-19 22:28:54 +0100

    trads

--- src/data/fr.js
+++ src/data/fr.js
@@ -26,14 +26,14 @@ module.exports = {
     contactInfo: "Si vous avez des questions ou des suggestions, n'hésitez pas à nous contacter par téléphone ou par email:",
     title: "Infos Pratiques",
     location: "L'événement aura lieu au Chalet de la Porte Jaune, l'adresse est Avenue de Nogent 75012 Paris. Le lieu est facilement accessible en voiture (parking gratuit devant) RER et Uber.",
-    giftInfo: "Si vous recherchez la liste de mariage, vous cherchiez peut être des idées de cadeaux. Bien que ça ne soit pas nécessaire, nous apprécions vos gestes généreux. Nous vivons ensemble depuis deux ans et demi et nous avons déjà tout ce qu'on peut imaginer. Plus que des 'objets' nous apprécierons une contribution pour des événements à venir comme des voyages, l'achat d'un logement ou fonder une famille!",
+    giftInfo: "Si vous recherchez la liste de mariage, vous cherchiez peut être des idées de cadeaux. Bien que ça ne soit pas nécessaire, nous apprécions vos gestes généreux. Nous vivons ensemble depuis deux ans et demi et nous avons déjà tout ce qu'on peut imaginer. Plus que des 'objets' nous apprécierons une contribution pour des événements à venir comme des voyages au Canada, l'achat d'un logement ou fonder une famille!",
     playlist: "Nous avons la salle jusqu'à 2h du matin et nous comptons sur votre participation pour créer la playlist officielle! Ajoutez des chansons à la liste pour créer la folle ambiance de la soirée! Suivez le lien ci dessous pour ajouter des chansons dans spotify.",
     spotify: "Ouvrir dans spotify",
     venueLink: "Visiter le site web du Chalet de la Porte Jaune.",
     googleMapsLink: "Localiser le lieu dans Google Maps.",
   },
   about: {
-    title: `Jeoff et Tobi, à l'action!`,
-    caption: "On s'est rencontrés dans une soirée années 80 à Toronto. Comme c'est souvent le cas à Toronto, la majorité des gens venait de l'étranger. Nous ne nous souvenons pas de tous les détails de notre première conversation, mais nous n'avons oublié aucun moments depuis. Déjà 5 ans passés ensemble, une vie dans deux continents complétée par de beaux voyages dans le monde entier. Le mariage est notre prochaine grande aventure!"
+    title: `Jeoff et Tobi, en l'action!`,
+    caption: "On s'est rencontrés dans une soirée années 80 à Toronto. Comme c'est souvent le cas à Toronto, la majorité des gens venaient de l'étranger. Nous ne nous souvenons pas de tous les détails de notre première conversation, mais nous n'avons oublié aucun moments depuis. Déjà 5 ans passés ensemble, une vie sur deux continents complétée par de beaux voyages dans le monde entier. Le mariage est notre prochaine grande aventure!"
   }
 };
